51e2f7e192073fd3f00f41352676bedf2e881f3b
LucasFlicky <lucasflicky@hotmail.com>
2017-01-30 22:25:23 -0200

    added mention support

--- core/commands/cook.js
+++ core/commands/cook.js
@@ -1,3 +1,7 @@
 exports.run = (bot, message, args, userData, caller, gear, points) => {
-        message.reply("voce tem " + userData.cookies + " cookies")
+ if(message.mentions.users.size === 0) {
+     return message.reply("voce tem " + userData.cookies + " cookies")
+    }
+  let target = message.mentions.users.first();
+     return message.channel.sendMessage(target.username +" tem " + points[target.id].cookies + " cookies")
     }
