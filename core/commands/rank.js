30e274d1cf25e17c6a48562004d9f9726c389ff8
LucasFlicky <lucasflicky@hotmail.com>
2017-02-02 21:40:48 -0200

    new rank

--- core/commands/rank.js
+++ core/commands/rank.js
@@ -1,11 +1,15 @@
+const Discord = require("discord.js");
 const arraySort = require('array-sort')
 const fs = require("fs");
-
+var paths = require("../paths.js");
 
 exports.run = (bot, message, args, userData, caller, gear, points) => {
 
-        console.log("RANK VIEW INVOKED by " + caller + "-------------\n")
-        var rankItem = []
+ emb =    new Discord.RichEmbed();
+
+
+
+     var rankItem = []
         var ranked = []
         for (var i in points) {
             rankItem.name = points[i].name
@@ -18,17 +22,49 @@ exports.run = (bot, message, args, userData, caller, gear, points) => {
             reverse: true
         })
         console.log(ranked)
-        let replyData = `
-:first_place: 1st   **${ranked[0].name}**  Level ${ranked[0].level} :: ${ranked[0].points} Exp
 
-:second_place: 2nd  **${ranked[1].name}** Level ${ranked[1].level} :: ${ranked[1].points} Exp
 
-:third_place: 3rd   **${ranked[2].name}**  Level ${ranked[2].level} :: ${ranked[2].points} Exp
+    emb.setColor('#da5bae')
+    emb.title = "Pink Hoodie SKYNET Maid Cafe Oldfags Ranking"
 
-:medal: 4th **${ranked[3].name}**    Level ${ranked[3].level} :: ${ranked[3].points} Exp
+    emb.setAuthor('Pollux',bot.user.avatarURL,'https://github.com/LucasFlicky/polluxbot')
 
-:medal: 5th **${ranked[4].name}**    Level ${ranked[4].level} :: ${ranked[4].points} Exp
+  emb.setFooter('Se você não aparece aqui, digite +profile para ver seu Level')
+ emb.setThumbnail("https://raw.githubusercontent.com/LucasFlicky/polluxbot/master/avis/skynet.png")
+ // emb.setImage("https://raw.githubusercontent.com/LucasFlicky/polluxbot/master/avis/skynet.png")
+  // emb.setImage("https://raw.githubusercontent.com/LucasFlicky/polluxbot/master/avis/2.png")
+    emb.description = "Os Top-5 fregueses mais tradicionais do Cyber Cafe"
 
-                        `
-        message.channel.sendMessage(replyData)
-    };
+
+      emb.addField(':first_place: 1st',ranked[0].name, true)
+
+      emb.addField('Level '+ranked[0].level,'**'+ranked[0].points + '** Exp', true)
+
+
+      emb.addField(':second_place: 2nd',ranked[1].name, true)
+
+      emb.addField('Level '+ranked[1].level,'**'+ranked[1].points + '** Exp', true)
+
+       emb.addField(':third_place: 3rd',ranked[2].name, true)
+
+      emb.addField('Level '+ranked[2].level,'**'+ranked[2].points + '** Exp', true)
+
+       emb.addField(':medal: 4th',ranked[3].name, true)
+
+      emb.addField('Level '+ranked[3].level,'**'+ranked[3].points + '** Exp', true)
+
+
+       emb.addField(':medal: 5th',ranked[4].name, true)
+
+      emb.addField('Level '+ranked[4].level,'**'+ranked[4].points + '** Exp', true)
+
+
+
+    message.channel.sendEmbed(emb)
+
+
+
+
+
+
+}
