bc766591ee0d91bc378cb62b0fb05726c59b216f
LucasFlicky <lucasflicky@hotmail.com>
2017-02-10 22:51:17 -0200

    test-command

--- core/commands/a.js
+++ core/commands/a.js
@@ -2,34 +2,38 @@ const Discord = require("discord.js");
 const arraySort = require('array-sort')
 const Jimp = require("jimp");
 const fs = require("fs");
-var paths = require("../paths.js");
 
 exports.run = (bot, message, args, userData, caller, gear, points, skynet) => {
-
-  emb = new Discord.RichEmbed();
-  
+const Server = message.Server
 
 
-        emb.setColor('#ffd156')
-    emb.title = "UPDATE Announcement"
+        var paths = require("../paths.js");
+        var m = require("../../utils/multilang");
 
 
-    emb.setAuthor('Pollux Maintainance',bot.user.avatarURL,'https://discord.gg/ay48h7Q')
-    emb.setThumbnail('https://github.com/LucasFlicky/polluxbot/blob/master/avis/4.gif?raw=true')
-    emb.setFooter('Use +mute_notices para desativar estes avisos. +unmute_notices para reativar.')
+        /* emb = new Discord.RichEmbed();
+         emb.setColor('#ffd156')
+         emb.title = "UPDATE Announcement"
+         emb.setAuthor('Pollux Maintainance', bot.user.avatarURL, 'https://discord.gg/ay48h7Q')
+         emb.setThumbnail('https://github.com/LucasFlicky/polluxbot/blob/master/avis/4.gif?raw=true')
+         emb.setFooter('Use +mute_notices para desativar estes avisos. +unmute_notices para reativar.')
+             //emb.setThumbnail("https://raw.githubusercontent.com/LucasFlicky/polluxbot/master/avis/display.png")
+             // emb.setImage("https://raw.githubusercontent.com/LucasFlicky/polluxbot/master/avis/2.png")
+             //emb.description = "Os Top-5 mais rubificadoss do server"
+         emb.description = ''
+         emb.addField('New or Updated Commands', 'a', true)
+         message.channel.sendEmbed(emb)*/
+
+                var mention = 'None'
+        var user = message.author
+        if (message.mentions.users.size > 0){
+            mention = message.mentions.users.first()
+            console.log(mention)
+
+        }
 
 
-  //emb.setThumbnail("https://raw.githubusercontent.com/LucasFlicky/polluxbot/master/avis/display.png")
-  // emb.setImage("https://raw.githubusercontent.com/LucasFlicky/polluxbot/master/avis/2.png")
-    //emb.description = "Os Top-5 mais rubificadoss do server"
-
-      emb.description = ''
-      emb.addField('New or Updated Commands','a', true)
 
 
-
-
-
-    message.channel.sendEmbed(emb)
-
-}
+        message.reply(m.call('ping','success', Server.language, {user:user, mention:mention}))
+        }
