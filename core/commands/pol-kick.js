9f739b970ccec01c1f26d1afd116d9a920273fd7
LucasFlicky <lucasflicky@hotmail.com>
2017-02-10 22:52:46 -0200

    vanilla pol-kick re-enabled

--- core/commands/pol-kick.js
+++ core/commands/pol-kick.js
@@ -1,6 +1,8 @@
-exports.run = (bot, message, args, userData, caller, gear, points, skynet) => {
+exports.run = (bot, message, args, userData, caller, gear, points, skynet, modules) => {
     const Jimp = require("jimp");
     var paths = require("../paths.js");
+
+
 
 
     if (message.mentions.users.size === 0) {
@@ -11,6 +13,11 @@ exports.run = (bot, message, args, userData, caller, gear, points, skynet) => {
     if (!kickMember) {
         return message.reply("deu ruim, não achei esse maluco");
     }
+    if (!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+        return message.reply("Por algum raio de razão eu não posso kickar esse cara. MIMDÁ PERMISSÃO").catch(console.error);
+    }
+
+
 
 
 
@@ -19,26 +26,34 @@ exports.run = (bot, message, args, userData, caller, gear, points, skynet) => {
         img = kik.avatarURL.substr(0, kik.avatarURL.length - 10);
     }
 
-    gear.roundify(img, caller)
-    setTimeout(function () {
-        Jimp.read(paths.ROUND + caller + '.png').then(function (face) {
+
+    Jimp.read(img).then(function (face) {
+        face.resize(126, 126)
+        Jimp.read(paths.BUILD + "note.png").then(function (lenna) {
+            face.mask(lenna, 0, 0)
+
+
             face.resize(96, 96)
             face.rotate(-45)
             Jimp.read(paths.BUILD + "jazz.png").then(function (jazz) {
                 jazz.composite(face, 80, 31);
-                jazz.write(`${paths.ROUND}/${caller}2.png`);
+                //jazz.write(`${paths.ROUND}/${caller}2.png`);
+                message.channel.sendMessage('Ok, me dá um segundo...')
+                jazz.getBuffer(Jimp.MIME_PNG, function (err, image) {
+
+
+                    message.channel.sendFile(image, 'kicked.png', `:boot: Meti um pé na bunda de ${kik.username}.`).then(m => {
+                      //  kickMember.kick()
+                    }).catch(console.error)
+                })
 
             });
 
         });
-    }, 800)
-    message.channel.sendMessage('Ok, me dá um segundo...')
-    setTimeout(function () {
-        message.channel.sendFile(`${paths.ROUND}/${caller}2.png`, 'kicked.png', `:boot: Meti um pé na bunda de ${kik.username}.`).then(m => {
-            //kickMember.kick()
-            message.channel.sendMessage('this was a test, no one actually kicked')
-        }).catch(console.error)
-    }, 3000)
+    });
+
+
+
 
 
 }
