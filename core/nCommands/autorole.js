814ca0f2c9bdf3b9875e99dcf455752ce7493537
Lucas Oliveira <lucas.oliveira@rbstv.com.br>
2017-06-27 14:14:56 -0300

    test

--- core/nCommands/autorole.js
+++ core/nCommands/autorole.js
@@ -54,61 +54,19 @@ var modPass=false
     }
 
 
-    if (message.mentions.roles.size === 0 && !MSG.includes('list') && args != 'l') {
-
-        return message.reply(mm('CMD.noRolesGiven', {          //TEMPORARY
-            lngs: LANG
-        })).catch(console.error);
-
-    }
-
-
-    if (args === "add" || args === "+"){
-
-
-        if (DB.get(Server.id).modules.AUTOROLES.includes(Target)){
-            return message.reply(mm('CMD.roleAlreadyHere', {          //TEMPORARY
-                lngs: LANG
-            })).catch(console.error);
-        }
-        gear.paramAdd (Server,"AUTOROLES",Target)
-
-        return message.reply(mm('CMD.roleAdded', {          //TEMPORARY
-            lngs: LANG
-        })).catch(console.error);
-
-    } // ADD
-
-
-    if (args === "rem" || args === "remove" || args === "-"){
-
-
-        if (!DB.get(Server.id).modules.AUTOROLES.includes(Target)){
-            return message.reply(mm('CMD.noRoleHere', {          //TEMPORARY
-                lngs: LANG
-            })).catch(console.error);
-        }
-        gear.paramRemove (Server,"AUTOROLES",Target)
-
-        return message.reply(mm('CMD.roleAdded', {          //TEMPORARY
-            lngs: LANG
-        })).catch(console.error);
-
-    } // REMOVE
-
 
     if (args === "list" || args === "l" ){
 
 
 
-        let autoroles = !DB.get(Server.id).modules.AUTOROLES
-        let output = ""
-        for (var i ; i < autoroles.length; i++){
+     //   let autoroles = !DB.get(Server.id).modules.AUTOROLES
+        let output = "aaa"
+      /*  for (var i ; i < autoroles.length; i++){
 
             output += autoroles[i].name+"\n"
 
         }
-
+*/
         message.reply('```\n'+output+'```')
 
     } // LIST
