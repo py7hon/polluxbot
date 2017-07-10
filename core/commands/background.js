fcf92e1977e64b97508b408f35fa0e5d26cee0b9
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-07-10 02:52:12 -0300

    new

--- core/commands/background.js
+++ core/commands/background.js
@@ -3,7 +3,7 @@ var cmd = 'background';
 var Jimp = require("jimp")
 
 var init = function (message,userDB,DB) {
-var args = message.content.split(1)[0]
+var args = message.content.split(" ").slice(1)[0]
 if (args == undefined || args.length<8){
 
 
@@ -16,7 +16,7 @@ if (args == undefined || args.length<8){
 
      Jimp.read("http://files.pollux.fun/"+args+".png", function (err, ovlay) {
                 if (err) {
-                    message.reply("INVALID IMAGE URL")
+                 return   message.reply("INVALID IMAGE URL")
                 }})
 //    if (message.author.id != '88120564400553984') return message.reply('Only my master can send me direct orders. now begone!');
 
