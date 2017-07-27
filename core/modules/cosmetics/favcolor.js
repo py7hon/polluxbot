0aea377e1b1e62a7e1666238775ed812ce5c107b
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-07-27 19:36:57 -0300

    fav

--- core/modules/cosmetics/favcolor.js
+++ core/modules/cosmetics/favcolor.js
@@ -1,50 +1,62 @@
-
 var gear = require("../../gearbox.js");
 var cmd = 'color';
 var paths = require("../../paths.js");
 
 
-var init = function (message,userDB,DB) {
-         var Server = message.guild;
-            var Channel = message.channel;
-            var Author = message.author;
-            if (Author.bot) return;
-            var Member = Server.member(Author);
-            var Target = message.mentions.users.first() || Author;
-            var MSG = message.content;
-            var bot = message.botUser
-            var args = MSG.split(' ').slice(1)[0]
-          //  var input = args[0].toUpperCase()
+var init = function (message, userDB, DB) {
+    var Server = message.guild;
+    var Channel = message.channel;
+    var Author = message.author;
+    if (Author.bot) return;
+    var Member = Server.member(Author);
+    var Target = message.mentions.users.first() || Author;
+    var MSG = message.content;
+    var bot = message.botUser
+    var args = MSG.split(' ').slice(1)[0]
+    //  var input = args[0].toUpperCase()
 
 
-            try{
-            //HELP TRIGGER
-    let helpkey = mm("helpkey",{lngs:message.lang})
-if (MSG.split(" ")[1]==helpkey || MSG.split(" ")[1]=="?"|| MSG.split(" ")[1]=="help"){
-    return gear.usage(cmd,message);
-}}catch(e){gear.hook.send(e.error)}
-//------------
-message.reply(gear.emoji("check"))
-            gear.paramDefine(Author,"favcolor",args)
-         var hex = parseInt((args+"FF").replace(/^#/, ''), 16);
+    try {
+        //HELP TRIGGER
+        let helpkey = mm("helpkey", {
+            lngs: message.lang
+        })
+        if (
+    isNaN(parseInt((args + "FF").replace(/^#/, ''), 16))||!args||args===undefined||MSG.split(" ")[1] == helpkey || MSG.split(" ")[1] == "?" || MSG.split(" ")[1] == "help") {
+            return gear.usage(cmd, message);
+        }
+    } catch (e) {
+        gear.hook.send(e.error)
+    }
+    //------------
 
-                var image = new gear.Jimp(126, 126,  hex, function (err, img) {
-                    gear.Jimp.read(paths.BUILD + "note.png").then(function (lenna) {
-                    img.mask(lenna, 0, 0)
-  img.getBuffer(gear.Jimp.MIME_PNG, function (err, image) {
-                    message.channel.send({files:[{attachment:image,name:"file.png"}]})
+    var hex = parseInt((args + "FF").replace(/^#/, ''), 16);
+
+    message.reply(gear.emoji("check"))
+    gear.paramDefine(Author, "favcolor", args)
+
+    var image = new gear.Jimp(126, 126, hex, function (err, img) {
+        gear.Jimp.read(paths.BUILD + "note.png").then(function (lenna) {
+            img.mask(lenna, 0, 0)
+            img.getBuffer(gear.Jimp.MIME_PNG, function (err, image) {
+                message.channel.send({
+                    files: [{
+                        attachment: image,
+                        name: "file.png"
+                    }]
+                })
 
 
 
-  })
-  })
+            })
+        })
 
-});
+    });
 
 
 }
- module.exports = {
-    pub:true,
+module.exports = {
+    pub: true,
     cmd: cmd,
     perms: 3,
     init: init,
